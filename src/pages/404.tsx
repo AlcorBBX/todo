d13696080e6ed0241993ff7bbc6fa62122d11a6b
AlcorBBX <lev.matisov55@gmail.com>
2022-12-29 09:51:34 +0500

    add card ui

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,53 @@
+import { Layout } from "../components/layout/Layout";
+import { Card } from "../components/ui/UIcards/main/MainCard";
+
 export const NotFound = () => {
   return (
+    <Layout>
+      <Card
+        // title='Card Title'
+        cover={
+          <img
+            alt='example'
+            src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+          />
+        }
+        actions={[<Actions />]}
+      >
+        <h1>Not found</h1>
+        <p>The page you&apos;re looking for was not found.</p>
+      </Card>
+
+      <Card title='Card Title' actions={[<Actions />]}>
+        <h1>Not found</h1>
+        <p>The page you&apos;re looking for was not found.</p>
+      </Card>
+
+      <Card actions={[<Actions />]}>
+        <h1>Not found</h1>
+        <p>The page you&apos;re looking for was not found.</p>
+      </Card>
+      <Card title='Title'>
+        <h1>Not found</h1>
+        <p>The page you&apos;re looking for was not found.</p>
+      </Card>
+
+      <Card>
+        <h1>Not found</h1>
+        <p>The page you&apos;re looking for was not found.</p>
+      </Card>
+
+      <Card>sdf</Card>
+    </Layout>
+  );
+};
+
+export const Actions = () => {
+  return (
     <>
-      <h1>Not found</h1>
-      <p>The page you&apos;re looking for was not found.</p>
+      <button>button</button>
+      <button>button</button>
+      <button>button</button>
     </>
   );
 };
