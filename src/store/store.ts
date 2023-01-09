6d2ac48eb53c698b382aac4d50a6d768d27f6e25
AlcorBBX <lev.matisov55@gmail.com>
2023-01-09 16:53:20 +0500

    add new color variables

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
-import { rootReducer } from './root-reducer';
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { rootReducer } from "./root-reducer";
+import { rootWatcher } from "./sagas/root-watcher";
 
-export const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware();
 
-export type TypeRootState = ReturnType<typeof rootReducer>
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootWatcher);
+
+export type TypeRootState = ReturnType<typeof rootReducer>;
